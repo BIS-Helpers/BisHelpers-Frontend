0738fecda39d176ed12b3cd53fad26a511c4b8bd
Abodawoud <yousefabodawoud@gmail.com>
2024-03-14 12:06:13 +0200

    [Add] our project structure

---
+++ bisHelpers/src/app/modules/landing-page/landing-page.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { FeaturesComponent } from './features/features.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    FaqsComponent,
+    FeaturesComponent
+  ],
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule
+  ]
+})
+export class LandingPageModule { }
