215625693734c8eb72ca3d4e410f4cb39ee0ea32
Abodawoud <yousefabodawoud@gmail.com>
2024-05-08 00:17:37 +0300

    add-header-landing-page

--- bisHelpers/src/app/modules/landing-page/landing-page.module.ts
+++ bisHelpers/src/app/modules/landing-page/landing-page.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 //Landing Components
+import { LandingPageComponent } from './landing-page.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { FeaturesComponent } from './features/features.component';
 import { FaqsComponent } from './faqs/faqs.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,8 +15,9 @@ import { FaqsComponent } from './faqs/faqs.component';
     AboutComponent,
     FaqsComponent,
     FeaturesComponent,
+    LandingPageComponent,
   ],
-  imports: [CommonModule, LandingPageRoutingModule],
+  imports: [CommonModule, LandingPageRoutingModule, SharedModule],
 })
 export class LandingPageModule {
   constructor() {
