6f0f1071180b2035ea76b647943a5e595045b1d1
Abodawoud <yousefabodawoud@gmail.com>
2024-05-15 11:13:57 +0300

    fix-carousel & fix-about

--- bisHelpers/src/app/modules/landing-page/about/carousel/carousel.component.ts
+++ bisHelpers/src/app/modules/landing-page/about/carousel/carousel.component.ts
@@ -1,75 +1,88 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-interface students{
-  name:string;
-  track:string;
-  imgUrl:string;
-  linkedin:string
-  twitter:string;
-  get:string;
- }
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
+interface students {
+  name: string;
+  track: string;
+  imgUrl: string;
+  linkedin: string;
+  twitter: string;
+  github?: string;
+}
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
   @ViewChild('carouselContainer') carouselContainer!: ElementRef;
+  currentTranslation: number = 0;
+  activeButtonIndex = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+
+  teamMembers: students[] = [
+    {
+      name: 'Hoda AbdelQader',
+      track: 'Front-End Developer',
+      imgUrl: 'assets/images/member-2.jpg',
+      linkedin: 'https://www.linkedin.com/in/hoda-abdelqader-/',
+      twitter: '',
+      github: 'https://github.com/Hodaabdelqader',
+    },
+    {
+      name: 'Yousef Abodawoud',
+      track: 'Front-End Developer',
+      imgUrl: 'assets/images/member-1.jpg',
+      linkedin: 'https://www.linkedin.com/in/abodawoud/',
+      twitter: 'https://twitter.com/Abodaawoud',
+      github: 'https://github.com/Abodawoud',
+    },
+    {
+      name: 'Yousef Ahmed',
+      track: 'Back-End Developer',
+      imgUrl: 'assets/images/member-5.jpg',
+      linkedin: 'https://www.linkedin.com/in/youssefahmed1/',
+      twitter: 'https://twitter.com/joe_ahmed_',
+      github: 'https://github.com/Youssef-AhmedX',
+    },
+    {
+      name: 'Ziad Ehab',
+      track: 'Back-End Developer',
+      imgUrl: 'assets/images/member-3.jpg',
+      linkedin: '',
+      twitter: '',
+      github: 'https://github.com/ZiadEhab15',
+    },
+    {
+      name: 'Mego',
+      track: 'Business Analyst',
+      imgUrl: 'assets/images/member-6.jpg',
+      linkedin: 'https://www.linkedin.com/in/ahmed-abdelmeged-86324b240/',
+      twitter: '',
+    },
+    {
+      name: 'Hefo',
+      track: 'Business Analyst',
+      imgUrl: 'assets/images/member-4.jpg',
+      linkedin: 'https://www.linkedin.com/in/abdulrahman-al-hefnawi-4022b21b6/',
+      twitter: '',
+    },
+  ];
+
+  moveCarousel(distance: number, index: number) {
+    this.currentTranslation = distance;
+    this.activeButtonIndex = index;
   }
 
-  teamMembers: students[]=[
-    {
-      name: "Hoda AbdelQader",
-      track: 'g',
-      imgUrl: 'assets/images/testimonial.jpg',
-      linkedin:'',
-      twitter:'',
-      get:''
-      
-    },
-    {
-      name: "Yousef abo dawoud",
-      track: 'f',
-      imgUrl: 'assets/images/testimonial.jpg',
-      linkedin:'',
-      twitter:'',
-      get:''
-    },
-    {
-      name: "Yousef Ahmed",
-      track: 'g',
-      imgUrl: 'assets/images/testimonial.jpg',
-      linkedin:'',
-      twitter:'',
-      get:''
-    },
-    {
-      name: "Mego",
-      track: 'f',
-      imgUrl: 'assets/images/testimonial.jpg',
-      linkedin:'',
-      twitter:'',
-      get:''
-    }
-    ,{
-      name: "Ziad",
-      track: 'f',
-      imgUrl: 'assets/images/testimonial.jpg',
-      linkedin:'',
-      twitter:'',
-      get:''
-    },
-    {
-      name: "Hefo",
-      track: 'f',
-      imgUrl: 'assets/images/testimonial.jpg',
-      linkedin:'',
-      twitter:'',
-      get:''
-    }
-  ];
-   }
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.moveCarousel(0, 0);
+  }
+}
