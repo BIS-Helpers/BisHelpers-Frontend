10f33491f87108856f2227f69a14f8411cfa8c41
Abodawoud <yousefabodawoud@gmail.com>
2024-05-22 12:06:44 +0300

    Convert line endings to LF

--- bisHelpers/src/app/modules/auth/signup/signup.component.ts
+++ bisHelpers/src/app/modules/auth/signup/signup.component.ts
@@ -1,41 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
-interface types {
-  name: string;
+interface gender {
+  type: string;
 }
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-
 export class SignupComponent implements OnInit {
   signupForm: FormGroup = new FormGroup({});
 
   datejoin: Date | undefined;
   datebirth: Date | undefined;
-  gender: types[] | undefined;
+  gender: gender[] | undefined;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.gender = [{ type: 'Male' }, { type: 'Female' }];
     this.signupForm = this.fb.nonNullable.group({
-      name: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(2)]],
       phone: ['', Validators.required],
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
       collageid: ['', Validators.required],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required],
       joinDate: ['', Validators.required],
       birthDate: ['', Validators.required],
-      gender: ['', Validators.required]
+      gender: ['', Validators.required],
     });
+  }
 
-    this.gender = [
-            { name: 'Male'},
-            { name: 'Female' },
-        ];
+  onSubmit() {
+    this.updateControlDate();
+    console.log(this.signupForm.value);
+  }
 
-}
+  updateControlDate() {
+    if (
+      this.signupForm.get('birthDate')?.value &&
+      this.signupForm.get('joinDate')?.value
+    ) {
+      let birthDate = this.signupForm.get('birthDate')?.value;
+      let joinDate = this.signupForm.get('joinDate')?.value;
+
+      birthDate = this.formateDate(birthDate);
+      joinDate = this.formateDate(joinDate);
+
+      this.signupForm.patchValue({ birthDate });
+      this.signupForm.patchValue({ joinDate });
+    }
+  }
+
+  formateDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const formatedDate = [year, month, day].join('-');
+    return formatedDate;
+  }
 }
